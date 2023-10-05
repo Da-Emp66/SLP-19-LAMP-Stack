510783de72332d2606f1ababc239636d17f9927c
Soham Ganguly <sohamg24@gmail.com>
2023-10-05 17:04:54 -0400

    Add CORS support for Create Contacts

--- api/CreateContact.php
+++ api/CreateContact.php
@@ -1,5 +1,9 @@
 <?php
 
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Session-Token");
+
 	require __DIR__ . '/HelperFunctions.php';
 
 	$inData = getRequestInfo();
