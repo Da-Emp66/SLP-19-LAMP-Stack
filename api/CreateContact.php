28c2fb87e1c30b864a6ee4b7cc5d4970575e2b0f
Soham Ganguly <sohamg24@gmail.com>
2023-10-05 16:35:16 -0400

    Fix CreateContact API to work with db

--- api/CreateContact.php
+++ api/CreateContact.php
@@ -4,16 +4,33 @@
 
 	$inData = getRequestInfo();
 	
-	$conn = new mysqli("localhost", "asher", "AmazingPassword2789", "COP4331_SLP19"); 	
+	$conn = new mysqli("localhost", "pandasaur", "izanagi", "COP4331_SLP19"); 	
 	if( $conn->connect_error )
 	{
-		returnWithFirstnameLastnameError( $conn->connect_error );
+		returnWithError( $conn->connect_error );
 	}
 	else
 	{
-		$stmt = $conn->prepare("INSERT INTO Contacts (SourceUserID,ContactUsername,ContactUserFirstName,ContactUserLastName,ContactUserEmail,ContactUserPhone) VALUES (?,?,?,?,?,?);");
-		$stmt->bind_param("ss", $inData["ID"], $inData["contactUsername"], $inData["contactUserFirstName"], $inData["contactUserLastName"], $inData["contactUserEmail"], $inData["contactUserPhone"]);
+		$userEmail = isset($_SERVER['HTTP_SESSION_TOKEN']) ? $_SERVER['HTTP_SESSION_TOKEN'] : '';
+		$stmt = $conn->prepare("INSERT INTO Contacts (ContactUsername,ContactFirstName,ContactLastName,ContactEmail,ContactPhone,SourceUserEmail) VALUES (?,?,?,?,?,?);");
+        $stmt->bind_param("ssssss", $inData["username"], $inData["firstName"], $inData["lastName"], $inData["email"], $inData["phoneNumber"], $userEmail);
 		$stmt->execute();
+
+		if ($stmt->affected_rows > 0) {
+			$stmt->close();
+
+			$successToken = 1;
+			echo $successToken;
+
+			$conn->close();
+			exit();
+		} else {
+			$stmt->close();
+			$conn->close();
+
+			returnWithError("Add Contact Failed.");
+		}
+
 		$result = $stmt->get_result();
 		$stmt->close();
 		$conn->close();
